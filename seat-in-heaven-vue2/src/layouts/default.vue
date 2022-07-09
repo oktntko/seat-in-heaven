4c37a0810aeee3c24eb9de008b7b53842345945f
oktntko <oktntko@gmail.com>
2022-07-09 23:42:52 +0900

    icons

--- seat-in-heaven-vue2/src/layouts/default.vue
+++ seat-in-heaven-vue2/src/layouts/default.vue
@@ -11,22 +11,7 @@
           </li>
           <li>
             <a href="#" class="mr-4 flex items-center hover:text-blue-100">
-              <span class="mr-1 inline-flex">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                  ></path>
-                </svg>
-              </span>
+              <Icon class="mr-1 inline-flex" icon="fe:logout" />
               Logout
             </a>
           </li>
@@ -57,22 +42,7 @@
               href="#"
               class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
             >
-              <span class="ml-4 inline-flex items-center justify-center">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                  ></path>
-                </svg>
-              </span>
+              <Icon class="ml-4 inline-flex items-center justify-center" icon="carbon:dashboard" />
               <span class="ml-2 truncate text-sm tracking-wide">Dashboard</span>
             </a>
           </li>
@@ -81,22 +51,10 @@
               href="#"
               class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
             >
-              <span class="ml-4 inline-flex items-center justify-center">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                  ></path>
-                </svg>
-              </span>
+              <Icon
+                class="ml-4 inline-flex items-center justify-center"
+                icon="iconoir:kanban-board"
+              />
               <span class="ml-2 truncate text-sm tracking-wide">Board</span>
               <span
                 class="ml-auto hidden rounded-full bg-indigo-50 px-2 py-0.5 text-xs font-medium tracking-wide text-blue-500 md:block"
@@ -109,22 +67,10 @@
               href="#"
               class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
             >
-              <span class="ml-4 inline-flex items-center justify-center">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                  ></path>
-                </svg>
-              </span>
+              <Icon
+                class="ml-4 inline-flex items-center justify-center"
+                icon="bx:message-rounded-error"
+              />
               <span class="ml-2 truncate text-sm tracking-wide">Messages</span>
             </a>
           </li>
@@ -133,22 +79,10 @@
               href="#"
               class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
             >
-              <span class="ml-4 inline-flex items-center justify-center">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  ></path>
-                </svg>
-              </span>
+              <Icon
+                class="ml-4 inline-flex items-center justify-center"
+                icon="ic:twotone-notification-add"
+              />
               <span class="ml-2 truncate text-sm tracking-wide">Notifications</span>
               <span
                 class="ml-auto hidden rounded-full bg-red-50 px-2 py-0.5 text-xs font-medium tracking-wide text-red-500 md:block"
@@ -166,22 +100,7 @@
               href="#"
               class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
             >
-              <span class="ml-4 inline-flex items-center justify-center">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  ></path>
-                </svg>
-              </span>
+              <Icon class="ml-4 inline-flex items-center justify-center" icon="bx:user-circle" />
               <span class="ml-2 truncate text-sm tracking-wide">Profile</span>
             </a>
           </li>
--- seat-in-heaven-vue2/src/main.ts
+++ seat-in-heaven-vue2/src/main.ts
@@ -1,9 +1,12 @@
+import { Icon } from "@iconify/vue2";
 import Vue from "vue";
 import App from "~/App.vue";
-import router from "~/router";
 import "~/main.css";
+import router from "~/router";
 
 Vue.config.productionTip = false;
+
+Vue.component("Icon", Icon);
 
 new Vue({
   router,
