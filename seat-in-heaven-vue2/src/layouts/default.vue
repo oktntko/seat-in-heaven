dce6db619a577c3ca3a89f42a1b360ca29dfc2db
oktntko <oktntko@gmail.com>
2022-07-10 22:56:20 +0900

    メニュー

--- seat-in-heaven-vue2/src/layouts/default.vue
+++ seat-in-heaven-vue2/src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex min-h-screen flex-auto flex-shrink-0 flex-col bg-white text-black antialiased dark:bg-gray-700 dark:text-white"
+    class="flex min-h-screen flex-auto flex-shrink-0 flex-col bg-gray-50 text-black antialiased dark:bg-gray-700 dark:text-white"
   >
     <!-- Header -->
     <div class="fixed z-10 ml-14 flex h-14 w-full items-center justify-between text-white md:ml-64">
@@ -32,78 +32,26 @@
 
       <div class="flex flex-grow flex-col justify-between overflow-y-auto overflow-x-hidden">
         <ul class="flex flex-col space-y-1 py-4">
-          <li class="hidden px-5 md:block">
-            <div class="flex h-8 flex-row items-center">
-              <div class="text-sm font-light uppercase tracking-wide text-gray-400">Main</div>
-            </div>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
-            >
-              <Icon class="ml-4 inline-flex items-center justify-center" icon="carbon:dashboard" />
-              <span class="ml-2 truncate text-sm tracking-wide">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
-            >
-              <Icon
-                class="ml-4 inline-flex items-center justify-center"
-                icon="iconoir:kanban-board"
-              />
-              <span class="ml-2 truncate text-sm tracking-wide">Board</span>
-              <span
-                class="ml-auto hidden rounded-full bg-indigo-50 px-2 py-0.5 text-xs font-medium tracking-wide text-blue-500 md:block"
-                >New</span
-              >
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
-            >
-              <Icon
-                class="ml-4 inline-flex items-center justify-center"
-                icon="bx:message-rounded-error"
-              />
-              <span class="ml-2 truncate text-sm tracking-wide">Messages</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
-            >
-              <Icon
-                class="ml-4 inline-flex items-center justify-center"
-                icon="ic:twotone-notification-add"
-              />
-              <span class="ml-2 truncate text-sm tracking-wide">Notifications</span>
-              <span
-                class="ml-auto hidden rounded-full bg-red-50 px-2 py-0.5 text-xs font-medium tracking-wide text-red-500 md:block"
-                >1.2k</span
-              >
-            </a>
-          </li>
-          <li class="hidden px-5 md:block">
-            <div class="mt-5 flex h-8 flex-row items-center">
-              <div class="text-sm font-light uppercase tracking-wide text-gray-400">Settings</div>
-            </div>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
-            >
-              <Icon class="ml-4 inline-flex items-center justify-center" icon="bx:user-circle" />
-              <span class="ml-2 truncate text-sm tracking-wide">Profile</span>
-            </a>
-          </li>
+          <template v-for="block in sidemenu">
+            <li :key="block.block_title" class="hidden px-5 md:block">
+              <div class="flex h-8 flex-row items-center">
+                <div class="text-sm font-light uppercase tracking-wide text-gray-400">
+                  {{ block.block_title }}
+                </div>
+              </div>
+            </li>
+            <template v-for="menu in block.menu">
+              <li :key="menu.title">
+                <router-link
+                  :to="menu.link"
+                  class="text-white-600 hover:text-white-800 relative flex h-11 flex-row items-center border-l-4 border-transparent pr-6 hover:border-blue-500 hover:bg-blue-800 focus:outline-none dark:hover:border-gray-800 dark:hover:bg-gray-600"
+                >
+                  <Icon class="ml-4 inline-flex items-center justify-center" :icon="menu.icon" />
+                  <span class="ml-2 truncate text-sm tracking-wide">{{ menu.title }}</span>
+                </router-link>
+              </li>
+            </template>
+          </template>
         </ul>
 
         <p class="hidden px-5 py-3 text-center text-xs md:block">Copyright @2021</p>
@@ -119,9 +67,68 @@
 
 <script lang="ts">
 import Vue from "vue";
+
+type MenuBlock = {
+  block_title: string;
+  menu: Menu[];
+};
+type Menu = {
+  title: string;
+  link: string;
+  icon: string;
+};
+
 export default Vue.extend({
   data() {
-    return {};
+    return {
+      sidemenu: [
+        {
+          block_title: "予約する",
+          menu: [
+            {
+              title: "15F",
+              link: "15",
+              icon: "15",
+            },
+            {
+              title: "16F",
+              link: "16",
+              icon: "16",
+            },
+          ],
+        },
+        {
+          block_title: "フロア管理",
+          menu: [
+            {
+              title: "15F",
+              link: "15",
+              icon: "15",
+            },
+            {
+              title: "16F",
+              link: "16",
+              icon: "16",
+            },
+          ],
+        },
+        {
+          block_title: "システム管理",
+          menu: [
+            {
+              title: "ユーザ管理",
+              link: "/system/users",
+              icon: "clarity:users-solid",
+            },
+            {
+              title: "フロア管理",
+              link: "/system/floors",
+              icon: "fa-solid:layer-group",
+            },
+          ],
+        },
+      ] as MenuBlock[],
+    };
   },
   methods: {},
 });
