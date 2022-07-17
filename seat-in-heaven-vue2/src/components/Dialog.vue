6313e2feb4dc17844b8002e9e9bd264e53b57ca6
oktntko <oktntko@gmail.com>
2022-07-18 00:37:53 +0900

    ダイアログ

---
+++ seat-in-heaven-vue2/src/components/Dialog.vue
@@ -0,0 +1,241 @@
+<template>
+  <div class="relative z-10" role="dialog" aria-modal="true">
+    <!-- Overlay -->
+    <transition
+      enter-class="opacity-0"
+      enter-active-class="ease-out duration-200"
+      enter-to-class="opacity-100"
+      leave-class="opacity-100"
+      :leave-active-class="`ease-in duration-${dissmissDuration}`"
+      leave-to-class="opacity-0"
+    >
+      <div
+        v-show="open"
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 backdrop-blur-[1px] transition-opacity"
+      ></div>
+    </transition>
+
+    <!-- Modal -->
+    <div class="fixed inset-0 z-10 overflow-y-auto" @click="handleOutsideClicked">
+      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <!-- Dialog -->
+        <transition
+          enter-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-active-class="ease-out duration-200"
+          enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+          leave-class="opacity-100 translate-y-0 sm:scale-100"
+          :leave-active-class="`ease-in duration-${dissmissDuration}`"
+          leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            v-show="open"
+            class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+            @click.stop
+          >
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                <div
+                  v-if="icon"
+                  :class="`mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-${color}-100 sm:mx-0 sm:h-10 sm:w-10`"
+                >
+                  <Icon :class="`h-6 w-6 text-${color}-600`" :icon="icon" />
+                </div>
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3
+                    v-if="title"
+                    id="modal-title"
+                    class="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    {{ title }}
+                  </h3>
+                  <div v-if="message" class="mt-2">
+                    <p class="text-sm text-gray-500">
+                      {{ message }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                ref="refConfirmButton"
+                type="button"
+                :class="`inline-flex min-w-[120px] justify-center rounded-md border border-transparent bg-${color}-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-${color}-700 focus:outline-none sm:ml-3 sm:w-auto sm:translate-y-0 sm:scale-95 sm:text-sm`"
+                @click="confirm"
+              >
+                {{ confirmText }}
+              </button>
+              <button
+                v-if="canCancel.button"
+                ref="refCancelButton"
+                type="button"
+                class="mt-3 inline-flex min-w-[120px] justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                @click="handleCancelButtonClicked"
+              >
+                {{ cancelText }}
+              </button>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+type DialogProps = Partial<{
+  type: string;
+  icon: string;
+  title: string;
+  message: string;
+  confirmText: string;
+  cancelText: string;
+  canCancel: {
+    escape: boolean;
+    button: boolean;
+    outside: boolean;
+  };
+  focusOn: string;
+}>;
+
+const Dialog = Vue.extend({
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: "info",
+      validator: (value: unknown) =>
+        typeof value === "string" && ["info", "success", "warning", "danger"].includes(value),
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    title: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    message: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    confirmText: {
+      type: String,
+      required: false,
+      default: "Confirm",
+    },
+    cancelText: {
+      type: String,
+      required: false,
+      default: "Cancel",
+    },
+    canCancel: {
+      type: Object as PropType<{
+        escape: boolean;
+        button: boolean;
+        outside: boolean;
+      }>,
+      required: false,
+      default: () => ({
+        button: true,
+        escape: true,
+        outside: true,
+      }),
+    },
+    focusOn: {
+      type: String,
+      required: false,
+      default: "Cancel",
+      validator: (value: unknown) => value === "Confirm" || value === "Cancel",
+    },
+  },
+  data() {
+    return {
+      open: false,
+      dissmissDuration: 200 /*ms*/,
+    };
+  },
+  computed: {
+    color(): string {
+      if (this.type === "success") {
+        return "green";
+      } else if (this.type === "warning") {
+        return "yellow";
+      } else if (this.type === "danger") {
+        return "red";
+      } else {
+        return "blue";
+      }
+    },
+  },
+  mounted() {
+    this.open = true;
+
+    document.addEventListener("keyup", this.handleEscapeKeyPress);
+
+    this.$nextTick(() => {
+      // $nextTick を入れないとフォーカスが移動しない
+      if (this.focusOn === "Confirm") {
+        (this.$refs.refConfirmButton as HTMLButtonElement).focus();
+      } else {
+        (this.$refs.refCancelButton as HTMLButtonElement).focus();
+      }
+    });
+  },
+  beforeDestroy() {
+    document.removeEventListener("keyup", this.handleEscapeKeyPress);
+  },
+  methods: {
+    handleCancelButtonClicked() {
+      if (this.canCancel.button) {
+        this.cancel();
+      }
+    },
+    handleEscapeKeyPress(event: KeyboardEvent) {
+      if (this.canCancel.escape && event.key === "Escape") {
+        this.cancel();
+      }
+    },
+    handleOutsideClicked() {
+      if (this.canCancel.outside) {
+        this.cancel();
+      }
+    },
+
+    cancel() {
+      this.open = false;
+      setTimeout(() => this.$emit("cancel"), this.dissmissDuration);
+    },
+    confirm() {
+      this.open = false;
+      setTimeout(() => this.$emit("confirm"), this.dissmissDuration);
+    },
+  },
+});
+
+export default Dialog;
+
+export function open(propsData: DialogProps) {
+  const ComponentClass = Vue.extend(Dialog);
+  const instance = new ComponentClass({ propsData }).$mount();
+  const parent = document.createElement("div");
+
+  parent.appendChild(instance.$el);
+  document.body.appendChild(parent);
+
+  return new Promise((resolve) => {
+    instance.$on("confirm", () => resolve({ result: true }));
+    instance.$on("cancel", () => resolve({ result: false }));
+  }).finally(() => {
+    instance.$destroy();
+    document.body.removeChild(parent);
+  });
+}
+</script>
+
+<style scoped></style>
